068db1ead26127d988f548d7068c538de93e3b45
eodameasante <emanodame1@gmail.com>
2017-06-18 13:46:31 +0100

    extra confi

--- clickFunctionality.js
+++ clickFunctionality.js
@@ -1,40 +1,74 @@
 function loadClickFunctionality() {
-
     const dom = document.querySelector('#base canvas:last-child');
     const sigmaCamera = s.camera;
 
-
-    dom.addEventListener("click", function (e) {
-
-        let x = sigma.utils.getX(e) - dom.offsetWidth / 2;
-        let y = sigma.utils.getY(e) - dom.offsetHeight / 2;
-
-        let p = sigmaCamera.cameraPosition(x, y);
-        x = p.x;
-        y = p.y;
-
-        s.graph.addNode({
-            id: (i++).toString(),
-            size: 10,
-            x: x,
-            y: y
-        });
-
-        s.refresh();
-
-    }, false);
-
-    s.bind("clickNode", function (e) {
-
-        s.graph.addEdge({
-            id : "edge",
-            source : "0",
-            target : "1"
-        });
-
-        s.refresh();
-    });
+    const nodesPresentOnGraph = [];
+    const nodesSelected = [];
 
     let i = 0;
 
+    addNodeOnLeftClick();
+    selectNodeOnShiftLeftClick();
+
+    function addNodeOnLeftClick() {
+
+        dom.addEventListener("click", function (e) {
+
+            if (!e.shiftKey) {
+
+                let x = sigma.utils.getX(e) - dom.offsetWidth / 2;
+                let y = sigma.utils.getY(e) - dom.offsetHeight / 2;
+
+                let p = sigmaCamera.cameraPosition(x, y);
+                x = p.x;
+                y = p.y;
+
+                const node = {
+                    id: createIdFromCoordinates(x, y),
+                    x: x,
+                    y: y,
+                    size: 10
+                };
+
+                nodesPresentOnGraph.push(node);
+
+                s.graph.addNode({
+                    id: node.id,
+                    x: node.x,
+                    y: node.y,
+                    size: node.size
+                });
+
+                s.refresh();
+            }
+        });
+    }
+
+    function selectNodeOnShiftLeftClick() {
+
+        s.bind("clickNode", function (e) {
+
+            if (e.data.captor.shiftKey) {
+
+                nodesSelected.push(e.data.node);
+
+                if (nodesSelected.length === 2) {
+
+                    s.graph.addEdge({
+                        id: (i++).toString(),
+                        source: nodesSelected[0].id,
+                        target: nodesSelected[1].id
+                    });
+
+                    nodesSelected.length = 0;
+                }
+
+                s.refresh();
+            }
+        });
+    }
+
+    function createIdFromCoordinates(x, y) {
+        return x + "." + "y";
+    }
 }
