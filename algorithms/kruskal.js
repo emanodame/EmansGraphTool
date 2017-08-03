e49a409c854646690a8fac5c23419707eb49f312
Eman <eman@Emans-MacBook.home>
2017-08-03 16:18:00 +0100

    readibility and performance improvements to Kruskal algorithm

--- algorithms/kruskal.js
+++ algorithms/kruskal.js
@@ -1,120 +1,120 @@
 (function () {
 
     sigma.classes.graph.addMethod('kruskal', function () {
-        const sortedEdges = returnSortedEdgesByWeight();
+        const sortedEdgesInGraph = returnSortedEdgesByWeight();
 
-        const treeArray = [];
-        const edgesToHighlight = [];
-        const setNodes = new Set();
+        const spanningTreesArray = [];
 
-        sortedEdges.forEach(function (edge) {
+        const edgesInMinSpanningTree = new Set();
+        const nodesInTrees = new Set();
 
-            if (treeArray.length === 0) {
-                const tree = new Tree();
+        initializeFirstTreeWithFirstEdge();
 
-                tree.add(edge.source);
-                tree.add(edge.target, edge.source);
-                treeArray.push(tree);
-                edgesToHighlight.push(edge.source + "|" + edge.target);
-                setNodes.add(edge.source);
-                setNodes.add(edge.target);
-            } else {
-                let makeNewTree = true;
-                for (let i = 0; i < treeArray.length; i++) {
+        for (let sortedEdgeIndex = 1; sortedEdgeIndex < sortedEdgesInGraph.length; sortedEdgeIndex++) {
 
-                    if (treeArray[i].contains(edge.source) && treeArray[i].contains(edge.target)) {
-                        console.log("same tree!");
-                        makeNewTree = false;
-                        break;
-                    }
+            if (!nodesInTrees.has(sortedEdgesInGraph[sortedEdgeIndex].source) &&
+                !nodesInTrees.has(sortedEdgesInGraph[sortedEdgeIndex].target)) {
+                addEdgeToNewSpanningTree(sortedEdgesInGraph[sortedEdgeIndex]);
+                continue;
+            }
 
-                    if (treeArray[i].contains(edge.source)) {
-                        let reset = false;
+            for (let spanningTree of spanningTreesArray) {
 
-                        if (setNodes.has(edge.target)) {
-                            for (let y = 0; y < treeArray.length; y++) {
-                                if (treeArray[i] === treeArray[y]) {
-                                    continue;
-                                }
-                                if (treeArray[y].contains(edge.target)) {
-
-                                    treeArray[y].traverseBFS(function (node) {
-                                        treeArray[i].add(node.data, edge.source)
-                                    });
-
-                                    treeArray.splice(y, 1);
-                                    reset = true;
-                                    break;
-                                }
-                            }
-                        }
-
-                        if (reset) {
-                            break;
-                        }
-
-                        edgesToHighlight.push(edge.source + "|" + edge.target);
-                        treeArray[i].add(edge.target, edge.source);
-                        setNodes.add(edge.source);
-                        setNodes.add(edge.target);
-                        makeNewTree = false;
-                        break;
-
-                    } else if (treeArray[i].contains(edge.target)) {
-                        let reset = false;
-
-                        if (setNodes.has(edge.source)) {
-                            for (let y = 0; y < treeArray.length; y++) {
-                                if (treeArray[i] === treeArray[y]) {
-                                    continue;
-                                }
-                                if (treeArray[y].contains(edge.source)) {
-
-                                    treeArray[y].traverseBFS(function (node) {
-                                        treeArray[i].add(node.data, edge.target)
-                                    });
-
-                                    treeArray.splice(y, 1);
-                                    reset = true;
-                                    break;
-                                }
-                            }
-                        }
-
-                        if (reset) {
-                            break;
-                        }
-
-                        edgesToHighlight.push(edge.source + "|" + edge.target);
-                        treeArray[i].add(edge.source, edge.target);
-                        setNodes.add(edge.source);
-                        setNodes.add(edge.target);
-                        makeNewTree = false;
-                        break;
-
-                    }
+                if (spanningTree.contains(sortedEdgesInGraph[sortedEdgeIndex].source) &&
+                    spanningTree.contains(sortedEdgesInGraph[sortedEdgeIndex].target)) {
+                    break;
                 }
 
-                if (makeNewTree) {
+                if (spanningTree.contains(sortedEdgesInGraph[sortedEdgeIndex].source)) {
 
-                    let tree1 = new Tree();
+                    if (nodesInTrees.has(sortedEdgesInGraph[sortedEdgeIndex].target)) {
 
-                    tree1.add(edge.source);
-                    tree1.add(edge.target, edge.source);
-                    setNodes.add(edge.source);
-                    setNodes.add(edge.target);
-                    edgesToHighlight.push(edge.source + "|" + edge.target);
-                    treeArray.push(tree1);
+                        for (let innerSpanningTree of spanningTreesArray) {
+
+                            if (innerSpanningTree.contains(sortedEdgesInGraph[sortedEdgeIndex].target)) {
+
+                                innerSpanningTree.traverseBFS(function (node) {
+                                    spanningTree.add(node.data, sortedEdgesInGraph[sortedEdgeIndex].source)
+                                });
+
+                                edgesInMinSpanningTree.add(sortedEdgesInGraph[sortedEdgeIndex].source + "|" + sortedEdgesInGraph[sortedEdgeIndex].target);
+                                spanningTreesArray.splice(spanningTreesArray.indexOf(innerSpanningTree), 1);
+                                break;
+                            }
+                        }
+
+                    } else {
+                        addEdgeToMinSpanningTreeAndVisitedNodes(sortedEdgesInGraph[sortedEdgeIndex]);
+                        spanningTree.add(sortedEdgesInGraph[sortedEdgeIndex].target, sortedEdgesInGraph[sortedEdgeIndex].source);
+                    }
+                    break;
+
+                } else if (spanningTree.contains(sortedEdgesInGraph[sortedEdgeIndex].target)) {
+
+                    if (nodesInTrees.has(sortedEdgesInGraph[sortedEdgeIndex].source)) {
+
+                        for (let innerSpanningTree of spanningTreesArray) {
+
+                            if (innerSpanningTree.contains(sortedEdgesInGraph[sortedEdgeIndex].source)) {
+
+                                innerSpanningTree.traverseBFS(function (node) {
+                                    spanningTree.add(node.data, sortedEdgesInGraph[sortedEdgeIndex].target)
+                                });
+
+                                edgesInMinSpanningTree.add(sortedEdgesInGraph[sortedEdgeIndex].source + "|" + sortedEdgesInGraph[sortedEdgeIndex].target);
+                                spanningTreesArray.splice(spanningTreesArray.indexOf(innerSpanningTree), 1);
+                                break;
+                            }
+                        }
+
+                    } else {
+                        addEdgeToMinSpanningTreeAndVisitedNodes(sortedEdgesInGraph[sortedEdgeIndex]);
+                        spanningTree.add(sortedEdgesInGraph[sortedEdgeIndex].source, sortedEdgesInGraph[sortedEdgeIndex].target);
+                    }
+                    break;
                 }
             }
-        });
+        }
 
-        return edgesToHighlight;
+        return edgesInMinSpanningTree;
+
+        function addEdgeToMinSpanningTreeAndVisitedNodes(sortedEdges) {
+            edgesInMinSpanningTree.add(sortedEdges.source + "|" + sortedEdges.target);
+
+            nodesInTrees.add(sortedEdges.source);
+            nodesInTrees.add(sortedEdges.target);
+        }
+
+        function addEdgeToNewSpanningTree(edge) {
+            const tree = new Tree();
+
+            tree.add(edge.source);
+            tree.add(edge.target, edge.source);
+
+            nodesInTrees.add(edge.source);
+            nodesInTrees.add(edge.target);
+
+            spanningTreesArray.push(tree);
+            edgesInMinSpanningTree.add(edge.source + "|" + edge.target);
+        }
+
+        function initializeFirstTreeWithFirstEdge() {
+            const tree = new Tree();
+
+            tree.add(sortedEdgesInGraph[0].source);
+            tree.add(sortedEdgesInGraph[0].target, sortedEdgesInGraph[0].source);
+
+            nodesInTrees.add(sortedEdgesInGraph[0].source);
+            nodesInTrees.add(sortedEdgesInGraph[0].target);
+
+            spanningTreesArray.push(tree);
+            edgesInMinSpanningTree.add(sortedEdgesInGraph[0].source + "|" + sortedEdgesInGraph[0].target);
+        }
+
+        function returnSortedEdgesByWeight() {
+            return s.graph.edges().sort(function (a, b) {
+                return a.label - b.label;
+            });
+        }
     });
-
-    function returnSortedEdgesByWeight() {
-        return s.graph.edges().sort(function (a, b) {
-            return a.label - b.label;
-        });
-    }
 }).call(window);
