91bc453a0a343e2bec5a6378be1ad51e41f35583
Emmanuel Odame-Asante <eman@Emmanuels-MacBook-Pro.local>
2017-08-29 15:17:13 +0100

    made dijsktraTeacher highlight edges too

--- teachers/dijsktraTeacher.js
+++ teachers/dijsktraTeacher.js
@@ -7,9 +7,19 @@ function executeDijkstraTeacher(path) {
     dijkstraTeacher();
 
     function dijkstraTeacher() {
-        const currentNode = path[counter];
+        const node1 = path[counter];
+        const node2 = path[counter + 1];
 
-        currentNode.color = "#6e0db6";
+        node1.color = "#6e0db6";
+        node2.color = "#6e0db6";
+
+        const edge = s.graph.edges(node1.id + "|" + node2.id);
+
+        if (edge) {
+            edge.color = "#6e0db6";
+        } else {
+            s.graph.edges(node2.id + "|" + node1.id).color = "#6e0db6";
+        }
 
         s.refresh();
         counter++;
