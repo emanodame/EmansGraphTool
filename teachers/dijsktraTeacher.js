45fa1bfa30e874e05059af371cf09f52a9141583
Eman Odame <emanodame@emans-mbp.home>
2018-02-12 23:37:05 +0000

    various improvements and updates

--- teachers/dijsktraTeacher.js
+++ teachers/dijsktraTeacher.js
@@ -1,34 +1,67 @@
+let dijkstraCounter = 0;
+const dijkstraEdgeStatesArray = [];
+
 function executeDijkstraTeacher(path) {
 
-    console.log("Dijkstra's Algorithm");
-
     const visitedNodes = new Set();
-    let counter = 0;
+
+    document.getElementById("helper-text").innerText = "";
 
     dijkstraTeacher();
 
     function dijkstraTeacher() {
-        const currentEdge = path[counter];
+        const currentEdge = path[dijkstraCounter];
+        const helperText = document.getElementById("helper-text");
+        const breakNode = document.createElement("br");
+
+        const edgeStates = {
+            count: dijkstraCounter,
+            edges: jQuery.extend(true, {}, s.graph.edges())
+        };
+
+        dijkstraEdgeStatesArray.push(edgeStates);
 
         if (!(visitedNodes.has(currentEdge.source) && (visitedNodes.has(currentEdge.target)))) {
             visitedNodes.add(currentEdge.source);
             visitedNodes.add(currentEdge.target);
+
+            const textNode = document.createTextNode("Connecting nodes " +
+                s.graph.nodes(currentEdge.source).label + " and " +
+                s.graph.nodes(currentEdge.target).label);
+
+            helperText.appendChild(textNode);
+            helperText.appendChild(breakNode);
             currentEdge.color = "#6e0db6";
-
-            console.log("Connecting nodes " +
-                s.graph.nodes(currentEdge.source).label + " and "
-                + s.graph.nodes(currentEdge.target).label);
-
-            console.log("From Start node, this has weight " + currentEdge.weight);
         } else {
-            console.log("This would be the next edge but the nodes have already been discovered!")
+            const textNode = document.createTextNode("This would be the next edge but the nodes have already been discovered!");
+
+            helperText.appendChild(textNode);
+            helperText.appendChild(breakNode);
         }
 
         s.refresh();
-        counter++;
+        dijkstraCounter++;
 
-        if (counter < path.length) {
+        if (dijkstraCounter < path.length) {
             startTimer(dijkstraTeacher);
         }
+
+        if (dijkstraCounter === visitedNodes.size) {
+            document.getElementById("pause-button").style.display = "none";
+            document.getElementById("play-button").style.display = "inline";
+        }
     }
+}
+
+function rewindBack() {
+    Object.values(dijkstraEdgeStatesArray[dijkstraCounter - 1].edges).forEach(function (edge) {
+        s.graph.edges(edge.id).color = edge.color;
+        s.refresh();
+    });
+
+    dijkstraCounter--;
+}
+
+function forwardOne() {
+    executeKruskalTeacher(ids).kruskalTeacher();
 }
--- teachers/kruskalTeacher.js
+++ teachers/kruskalTeacher.js
@@ -1,27 +1,67 @@
-function executeKruskalTeacher(idsOfMinSpanningTreeEdges) {
+let kruskalTeacherCount = 0;
+const kruskalEdgeStatesArray = [];
+let ids = [];
 
+function executeKruskalTeacher(idsOfMinSpanningTreeEdges) {
+    ids = idsOfMinSpanningTreeEdges;
     const sortedEdgesOnGraph = returnSortedEdgesByWeight();
 
-    let counter = 0;
+    document.getElementById("helper-text").innerText = "";
 
     kruskalTeacher();
 
     function kruskalTeacher() {
-        const currentEdge = sortedEdgesOnGraph[counter];
+        const currentEdge = sortedEdgesOnGraph[kruskalTeacherCount];
+        const helperText = document.getElementById("helper-text");
+        const breakNode = document.createElement("br");
+
+        const edgeStates = {
+            count: kruskalTeacherCount,
+            edges: jQuery.extend(true, {}, s.graph.edges())
+        };
+
+        kruskalEdgeStatesArray.push(edgeStates);
 
         if ($.inArray(currentEdge.id, idsOfMinSpanningTreeEdges) !== -1) {
-            document.getElementById("helper-text").innerText = currentEdge.label + " will be added to min spanning tree";
+            const textNode = document.createTextNode(currentEdge.label + " will be added to min spanning tree");
+
+            helperText.appendChild(textNode);
+            helperText.appendChild(breakNode);
             currentEdge.color = "#6e0db6";
+
         } else {
-            document.getElementById("helper-text").innerText = currentEdge.label + " will not be added to min spanning tree as a cycle will occur";
+            const textNode = document.createTextNode(currentEdge.label + "  will not be added to min spanning tree as a cycle will occur");
+
+            helperText.appendChild(textNode);
+            helperText.appendChild(breakNode);
             currentEdge.color = "#B6001E";
         }
 
         s.refresh();
-        counter++;
+        kruskalTeacherCount++;
 
-        if (counter < sortedEdgesOnGraph.length) {
+        if (kruskalTeacherCount < sortedEdgesOnGraph.length) {
             startTimer(kruskalTeacher);
         }
+
+        if (kruskalTeacherCount === sortedEdgesOnGraph.length) {
+            document.getElementById("pause-button").style.display = "none";
+            document.getElementById("play-button").style.display = "inline";
+        }
+
+        executeKruskalTeacher.kruskalTeacher = kruskalTeacher;
     }
+}
+
+function rewindBack() {
+    Object.values(kruskalEdgeStatesArray[kruskalTeacherCount - 1].edges).forEach(function (edge) {
+        s.graph.edges(edge.id).color = edge.color;
+        s.refresh();
+    });
+
+    kruskalTeacherCount--;
+}
+
+function forwardOne() {
+    executeKruskalTeacher(ids).kruskalTeacher();
 }
